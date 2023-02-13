9fab05865499fc1b9442e5d5c22d753055df427a
semanurbilada <semanurbilada@hotmail.com>
2023-02-13 15:43:21 +0300

    design & component changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,20 @@
 export default function Home() {
+    const deneme = console.log("home page...");
+    
     return(
-        <section className="h-screen flex items-center justify-center text-3xl">
+        <section className="container min-h-screen md:my-40 my-36 flex flex-col gap-20 items-center justify-center text-3xl">
             <h1 className="text-greenery-900">Information Systems Engineering Student!</h1>
-        </section>
 
+            <p className={`text-lg ${deneme}`}>
+            The web is a vast and powerful tool. Over the course of a few decades, <br></br> the internet has changed the way we work, the way we play and the way we interact with one another.
+            <br></br>Depending on how it’s used, it bridges nations, drives commerce, nurtures relationships, drives the innovation engine of the future and is responsible for more memes than we know what to do with.  <br></br>
+            It’s important that everyone has access to the web, but it’s also vital  <br></br>that we all understand the tools we use to access it. We use web browsers like Mozilla Firefox, Google Chrome, Microsoft Edge and Apple Safari every day, but do we understand what they are and how they work? In a short period of time we’ve gone from being amazed by the ability to send an email to someone around the world, to a change in how we think of information. It’s not a question of how much you know anymore, but simply a question of what browser or app can get you to that information fastest.
+            The web is a vast and powerful tool. Over the course of a few decades, <br></br> the internet has changed the way we work, the way we play and the way we interact with one another.
+            <br></br>Depending on how it’s used, it bridges nations, drives commerce, nurtures relationships, drives the innovation engine of the future and is responsible for more memes than we know what to do with.  <br></br>
+            It’s important that everyone has access to the web, but it’s also vital  <br></br>
+            The web is a vast and powerful tool. Over the course of a few decades, <br></br> the internet has changed the way we work, the way we play and the way we interact with one another.
+            <br></br>Depending on how it’s used, it bridges nations, drives commerce, nurtures relationships, drives the innovation engine of the future and is responsible for more memes than we know what to do with.  <br></br>
+            </p>
+        </section>
     );
 }
