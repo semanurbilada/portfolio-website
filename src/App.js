43f4c5bd750db865dcc21e31ae98036986ce8f5a
semanurbilada <semanurbilada@hotmail.com>
2023-02-13 15:22:50 +0300

    animated-gradient background designed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
 import Home from "./pages/Home.js";
 import Projects from "./pages/Projects.js";
 import About from "./pages/About.js";
 import Contact from "./pages/Contact.js";
+
 import Navbar from "./components/Navbar";
-
-
+import './css/index.css';
 
 
 export default function App() {
   return (
-    <div className="w-full overflow-hidden">
+    <div className="custom-background w-full overflow-hidden">
       <div className="container">
         <Router>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home />} exact />
+            <Route path="/" element={<Home /> } />
             <Route path="/projects" element={<Projects />}/>
             <Route path="/about" element={<About />}/>
             <Route path="/contact" element={<Contact />}/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,13 +20,28 @@ module.exports = {
           DEFAULT: "1rem",
         },
       },
+      'animation': {
+        'gradient-x':'gradient-x 6s ease infinite',
+      },
+      'keyframes': {
+        'gradient-x': {
+          '0%, 100%': {
+            'background-size':'180% 180%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size':'180% 180%',
+            'background-position': 'right center'
+          }
+        }
+      },
       colors: {
         greenery: {
-          default: "#aef9e0", //custom
+          default: "#aef9e0", //custom -> bg
           transparent: "#aef9e080", //custom
-          100: '#d1fae5', //background color
+          100: '#d1fae5', //bg
           300: '#6ee7b7',
-          500: '#10b981', 
+          500: '#10b981', //bg
           700: '#047857',
           900: '#134e4a'
         },
