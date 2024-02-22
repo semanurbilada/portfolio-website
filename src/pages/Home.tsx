da0ecddd2a67afb933a3a5c71cc86bee7280668c
semanurbilada <semanurbilada@hotmail.com>
2024-02-22 14:16:13 +0300

    Minor changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,14 +4,17 @@ const Home: React.FC = () => {
     
     return(
         <div className="page-default flex flex-col items-center justify-center">
-            <div className="h-screen flex md:flex-row flex-col items-center justify-center text-3xl md:gap-0 gap-8">
+            <div className="h-screen flex md:flex-row flex-col items-center justify-center text-3xl md:gap-2 gap-8">
                 <img
                     className="rounded-full md:w-60 md:h-60 w-44 h-44 md:hover:scale-105 md:cursor-pointer animation md:top-60 top-0 object-cover"
-                    src={`${process.env.PUBLIC_URL}/profil.png`} alt="profil"
+                    src={`${process.env.PUBLIC_URL}/assets/profil.png`} 
+                    alt="profil"
                 />
-                <h1 className="font-fira md:text-end text-center text-3xl tracking-wide md:leading-10">
+
+                <h1 className="font-fira md:text-end text-center md:text-3xl text-xl tracking-wide md:leading-10">
                     Information Systems Engineering Student who currently
-                    interested with <span className=" bg-greenery-default italic"> Front-End</span> & <span className="p-1 bg-greenery-default italic"> Robotics !</span>
+                    interested with; <br />
+                    <span className=" bg-greenery-default italic"> Front-End</span>, <span className="p-1 bg-greenery-default italic"> Robotics</span> & <span className="p-1 bg-greenery-default italic"> Aerospace !</span>
                 </h1>
             </div>
 
