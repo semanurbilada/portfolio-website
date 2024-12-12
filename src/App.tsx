cdd13c84453447cfd94f92f8135bb7930bfef0de
semanurbilada <semanurbilada@hotmail.com>
2024-12-12 11:00:02 +0300

    Add CV download button to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Route, Routes, HashRouter as Router } from "react-router-dom";
 
 import Home from "./pages/Home.tsx";
@@ -6,12 +7,11 @@ import About from "./pages/About.tsx";
 import Contact from "./pages/Contact.tsx";
 
 import Navbar from "./components/Navbar.tsx";
-import React from "react";
 
 function App() {
   return (
     <div className="custom-background w-full overflow-hidden">
-      <div className="container flex flex-col gap-10 md:gap-0 items-center justify-center">
+      <div className="container relative flex flex-col gap-10 md:gap-0 items-center justify-center">
         <Router>
           <Navbar />
           <Routes>
@@ -27,6 +27,18 @@ function App() {
             <li>Copyright © 2024 | Made by Semanur Bilada</li>
           </ul>
         </footer>
+
+        {/* Download CV Button */} 
+        {/* TODO: Responsive adjustments! */}
+        <div className="absolute bottom-10 right-0 hover:scale-110 animation shadow-2xl z-50">
+          <a
+            href="/assets/CV-Semanur-Bilada.pdf"
+            download="CV-Semanur-Bilada.pdf"
+            className="py-3 px-4 bg-gradient-to-r from-greenery-900 to-greenery-700 rounded-full text-greenery-100"
+          >
+            Download CV
+          </a>
+        </div>
       </div>
     </div>
   );
