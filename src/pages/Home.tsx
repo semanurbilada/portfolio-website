de992287fb642009c1ff8189cf074342568addbe
semanurbilada <semanurbilada@hotmail.com>
2024-12-27 22:08:00 +0300

    Add dynamic content in Home page layout, config later

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Home: React.FC = () => {
-    
-    return(
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const contentArray = [
+        {
+            h1: (
+                <>
+                    Information Systems Engineering Student who currently interested with; <br />
+                    <span className="bg-greenery-default italic">Front-End</span>,{" "}
+                    <span className="p-1 bg-greenery-default italic">Robotics</span> &{" "}
+                    <span className="p-1 bg-greenery-default italic">Aerospace !</span>
+                </>
+            ),
+        },
+        {
+            h1: (
+                <>
+                    Technology that I use:<br />
+                    <span className="bg-greenery-default italic">React</span>,{" "}
+                    <span className="p-1 bg-greenery-default italic">Tailwind</span> &{" "}
+                    <span className="p-1 bg-greenery-default italic"> Python</span>
+                    {/* tech's logos */}
+                </>
+            ),
+        },
+    ];
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentIndex((prev) => (prev + 1) % contentArray.length);
+        }, 3000);
+
+        return () => clearInterval(intervalId);
+    }, [contentArray.length]);
+
+    return (
         <div className="page-default flex flex-col items-center justify-center">
-            <section className="h-screen flex md:flex-row flex-col items-center justify-center text-3xl md:gap-2 gap-8">
+            <section className="h-screen w-full flex md:flex-row flex-col items-center justify-between text-3xl">
                 <img
                     className="rounded-full md:w-60 md:h-60 w-44 h-44 md:hover:scale-105 md:cursor-pointer animation md:top-60 top-0 object-cover"
-                    src={`${process.env.PUBLIC_URL}/assets/profil.png`} 
-                    alt="profil"
+                    src={`${process.env.PUBLIC_URL}/assets/profil.png`}
+                    alt="profile"
                 />
 
-                <h1 className="font-fira md:text-end text-center md:text-3xl text-xl tracking-wide md:leading-10">
-                    Information Systems Engineering Student who currently
-                    interested with; <br />
-                    <span className=" bg-greenery-default italic"> Front-End</span>, <span className="p-1 bg-greenery-default italic"> Robotics</span> & <span className="p-1 bg-greenery-default italic"> Aerospace !</span>
-                </h1>
+                {/* Dynamic content */}
+                <div>
+                    <h1 className="font-fira md:text-end text-center md:text-3xl text-xl tracking-wide md:leading-10">
+                        {contentArray[currentIndex].h1}
+                    </h1>
+                </div>
             </section>
-
-            {/* <section className="h-screen flex">
-                <h2>The TECH that I use in my projects:</h2>
-            </section> */}
         </div>
     );
-}
+};
 
 export default Home;
