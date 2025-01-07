f2439f9f392c8c012db2456085e62e5676f2d36c
semanurbilada <semanurbilada@hotmail.com>
2025-01-07 17:04:23 +0300

    Remove unnecessary component

--- src/App.tsx
+++ src/App.tsx
@@ -27,18 +27,6 @@ function App() {
             <li>Copyright © 2024 | Made by Semanur Bilada</li>
           </ul>
         </footer>
-
-        {/* Download CV Button */} 
-        {/* TODO: Responsive adjustments! */}
-        <div className="absolute bottom-10 right-0 hover:scale-110 animation shadow-2xl z-50">
-          <a
-            href="/assets/CV-Semanur-Bilada.pdf"
-            download="CV-Semanur-Bilada.pdf"
-            className="py-3 px-4 bg-gradient-to-r from-greenery-900 to-greenery-700 rounded-full text-greenery-100"
-          >
-            Download CV
-          </a>
-        </div>
       </div>
     </div>
   );
