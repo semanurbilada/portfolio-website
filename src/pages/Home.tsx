94d23e3312e366e21699f06c585e0124c2d708a7
semanurbilada <semanurbilada@hotmail.com>
2025-01-27 18:47:22 +0300

    Disable dynamic content in Home page for now

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -37,7 +37,7 @@ const Home: React.FC = () => {
 
     return (
         <div className="page-default flex flex-col items-center justify-center">
-            <section className="h-screen w-full flex md:flex-row flex-col items-center justify-between text-3xl">
+            <section className="h-screen flex md:flex-row flex-col items-center justify-center text-3xl md:gap-2 gap-8">
                 <img
                     className="rounded-full md:w-60 md:h-60 w-44 h-44 md:hover:scale-105 md:cursor-pointer animation md:top-60 top-0 object-cover"
                     src={`${process.env.PUBLIC_URL}/assets/profil.png`}
@@ -47,7 +47,11 @@ const Home: React.FC = () => {
                 {/* Dynamic content */}
                 <div>
                     <h1 className="font-fira md:text-end text-center md:text-3xl text-xl tracking-wide md:leading-10">
-                        {contentArray[currentIndex].h1}
+                        {/* {contentArray[currentIndex].h1} */}
+                        Information Systems Engineering Student who currently interested with; <br />
+                        <span className="bg-greenery-default italic">Front-End</span>,{" "}
+                        <span className="p-1 bg-greenery-default italic">Robotics</span> &{" "}
+                        <span className="p-1 bg-greenery-default italic">Aerospace !</span>
                     </h1>
                 </div>
             </section>
