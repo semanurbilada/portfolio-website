b3dec8c1f8253760a069896227fca8843e7e000a
semanurbilada <semanurbilada@hotmail.com>
2025-02-20 21:10:26 +0300

    Minor edits in About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,12 +4,12 @@ const About: React.FC = () => {
     return(
         <div className="page-default flex flex-col md:flex-row gap-20 md:gap-8 items-center justify-evenly">
             {/* Experience Timeline */}
-            <div className="md:w-2/3"> {/*TODO: title should centered?*/}
+            <div className="md:w-2/3"> {/*TODO: title should centered or not exist?*/}
                 <h2 className="text-center w-full text-lg md:text-xl font-semibold mt-8 mb-4">Experience & Education</h2>
                 <ol className="relative text-start text-sm md:text-base border-s border-gray-700">  
-                    <li className="mb-10 mt-5 ms-4 text-start">
+                    <li className="mb-10 mt-0 ms-4 text-start">
                         <div className="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border-s border-gray-900 bg-gray-700"></div>
-                        <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">July 2024 - August 2024</time>
+                        <time className="text-sm font-normal leading-none text-gray-400 dark:text-gray-500">July 2024 - August 2024</time>
                         <h3 className="about-title text-start"> 
                             <a href="https://deltav.com.tr/en/" target="_blank" rel="noreferrer">ENGINEER INTERN - DeltaV Space Technologies</a>
                         </h3>
@@ -18,7 +18,7 @@ const About: React.FC = () => {
 
                     <li className="mb-10 ms-4 text-start">
                         <div className="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border-s border-gray-900 bg-gray-700"></div>
-                        <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">December 2023 - May 2023</time>
+                        <time className="text-sm font-normal leading-none text-gray-400 dark:text-gray-500">December 2023 - May 2023</time>
                         <h3 className="about-title">
                             <a href="https://pirireis.edu.tr/en/" target="_blank" rel="noreferrer">STUDENT ASSISTANT - Piri Reis University</a> - Faculty of Engineering
                         </h3>
@@ -26,7 +26,7 @@ const About: React.FC = () => {
 
                     <li className="mb-10 ms-4 text-start">
                         <div className="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border-s border-gray-900 bg-gray-700"></div>
-                        <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February 2023 - November 2023</time>
+                        <time className=" text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February 2023 - November 2023</time>
                         <h3 className="about-title">
                             <a href="https://www.t3vakfi.org/en/" target="_blank" rel="noreferrer">RISING STAR SCHOLARSHIP - T3 Foundation</a>
                         </h3>
@@ -36,7 +36,7 @@ const About: React.FC = () => {
 
                     <li className="-mb-14 ms-4 text-start">
                         <div className="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border-s border-gray-900 bg-gray-700"></div>
-                        <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">July 2023 - August 2023</time>
+                        <time className="text-sm font-normal leading-none text-gray-400 dark:text-gray-500">July 2023 - August 2023</time>
                         <h3 className="about-title"> 
                             <a href="https://intellium.com.tr" target="_blank" rel="noreferrer">SUMMER INTERN - Intellium</a>
                         </h3>
