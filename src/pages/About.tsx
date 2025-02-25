7c5995c2b35d34a50e94b7dc622dabe86dd3db21
semanurbilada <semanurbilada@hotmail.com>
2025-02-25 10:45:05 +0300

    Edits in About page title

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const About: React.FC = () => {
     return(
-        <div className="page-default flex flex-col md:flex-row gap-20 md:gap-8 items-center justify-evenly">
+        <div className="page-default flex flex-col md:flex-row gap-28 md:gap-8 items-center justify-evenly">
             {/* Experience Timeline */}
             <div className="md:w-2/3"> {/*TODO: title should centered or not exist?*/}
-                <h2 className="text-center w-full text-lg md:text-xl font-semibold mt-8 mb-4">Experience & Education</h2>
+                {/* <h2 className="text-center w-full text-lg md:text-xl font-semibold mt-8 mb-4">Experience & Education</h2> */}
                 <ol className="relative text-start text-sm md:text-base border-s border-gray-700">  
                     <li className="mb-10 mt-0 ms-4 text-start">
                         <div className="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border-s border-gray-900 bg-gray-700"></div>
@@ -46,15 +46,19 @@ const About: React.FC = () => {
             </div>
 
             {/* Education Section */}
-            <div className="md:w-1/3 bg-greenery-500 rounded-full p-8 shadow-2xl">
-                <h3 className="md:text-base text-sm font-semibold">
-                    <a href="https://pirireis.edu.tr/en/" target="_blank" rel="noreferrer">
-                        Piri Reis University
-                    </a> 
-                </h3>
-                <p className="text-greenery-100 text-sm">2021 - Present</p>
-                <p className="about-title text-center md:text-base text-sm font-semibold mt-1">B.Sc. in Information Systems Engineering</p>
-                <p className="about-p text-greenery-100 text-center text-sm">Senior Year Student | AI, Robotics and Autonomy Projects</p>
+            <div className="md:w-1/3 flex flex-col items-center justify-center md:gap-20 gap-0">
+                <h2 className="hidden md:block text-center w-full text-lg md:text-xl font-semibold">Experience & Education Details</h2>
+
+                <div className="bg-greenery-500 rounded-full p-8 shadow-2xl">
+                    <h3 className="md:text-base text-sm font-semibold">
+                        <a href="https://pirireis.edu.tr/en/" target="_blank" rel="noreferrer">
+                            Piri Reis University
+                        </a> 
+                    </h3>
+                    <p className="text-greenery-100 text-sm">2021 - Present</p>
+                    <p className="about-title text-center md:text-base text-sm font-semibold mt-1">B.Sc. in Information Systems Engineering</p>
+                    <p className="about-p text-greenery-100 text-center text-sm">Senior Year Student | AI, Robotics and Autonomy Projects</p>
+                </div>
             </div>
         </div>
     );    
